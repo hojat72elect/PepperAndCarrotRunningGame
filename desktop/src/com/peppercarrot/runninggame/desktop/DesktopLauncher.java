1e0a72700957f8cc3f881721ac5f7ff6dc7c4694
hojat72elect <hojat72elect@gmail.com>
2024-11-07 02:41:58 -0500

    Fixed lots of build issues.

--- desktop/src/com/peppercarrot/runninggame/desktop/DesktopLauncher.java
+++ desktop/src/com/peppercarrot/runninggame/desktop/DesktopLauncher.java
@@ -2,19 +2,18 @@ package com.peppercarrot.runninggame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.peppercarrot.runninggame.PaCGame;
 import com.peppercarrot.runninggame.utils.Constants;
 
 public class DesktopLauncher {
-	public static void main(String[] arg) {
-		final LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Pepper and Carrot Running Game";
-		config.width = Constants.VIRTUAL_WIDTH;
-		config.height = Constants.VIRTUAL_HEIGHT;
-		new LwjglApplication(new PaCGame(), config);
+    public static void main(String[] arg) {
+        final LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Pepper and Carrot Running Game";
+        config.width = Constants.VIRTUAL_WIDTH;
+        config.height = Constants.VIRTUAL_HEIGHT;
+        new LwjglApplication(new PaCGame(), config);
 
-		// TODO: to generate a texture atlas
-		//TexturePacker.process("skin/", "skin/", "skin");
-	}
+        // TODO: to generate a texture atlas
+        //TexturePacker.process("skin/", "skin/", "skin");
+    }
 }
